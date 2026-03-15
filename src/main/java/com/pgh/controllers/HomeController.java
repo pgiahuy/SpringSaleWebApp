0d185a348860ff1276e538c5b78a16ceb757fc20
unknown <huyphan2792005@gmail.com>
2026-03-15 16:31:56 +0700

    done BTH2

--- src/main/java/com/pgh/controllers/HomeController.java
+++ src/main/java/com/pgh/controllers/HomeController.java
@@ -4,12 +4,15 @@
  */
 package com.pgh.controllers;
 
+import com.pgh.pojo.Category;
 import com.pgh.service.CategoryService;
 import com.pgh.service.ProductService;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.condition.ParamsRequestCondition;
@@ -20,6 +23,7 @@ import org.springframework.web.servlet.mvc.condition.ParamsRequestCondition;
  */
 
 @Controller
+@ControllerAdvice
 public class HomeController {
     @Autowired
     private CategoryService cateService;
@@ -27,9 +31,13 @@ public class HomeController {
     @Autowired
     private ProductService productService;
     
+    @ModelAttribute
+    public void commonResponse(Model model){
+        model.addAttribute("categories", cateService.getCates());
+    }
+    
     @RequestMapping("/")
     public String index(Model model, @RequestParam Map<String,String> params){
-        model.addAttribute("categories", this.cateService.getCates());
         
         model.addAttribute("products", this.productService.getProducts(params));
         return "index";
---
+++ src/main/java/com/pgh/controllers/ProductController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pgh.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author admin
+ */
+
+@Controller
+@RequestMapping("/admin")
+public class ProductController {
+    
+    @GetMapping("/products")
+    public String ceatrView(){
+        return "products";
+    }
+    
+}
